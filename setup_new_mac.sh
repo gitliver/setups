012fbd1f3fdbc44d9495906a7c41df627be26097
gitliver <oe2118@cumc.columbia.edu>
2020-03-12 22:48:28 -0400

    setup_new_mac.sh: update

--- setup_new_mac.sh
+++ setup_new_mac.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # A script for installing programs on a new Mac
-# June 1, 2017
+# 2020
+
+echo "get dev tools (annoying)"
+xcode-select --install
 
 echo "*** install brew ***"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-for i in wget coreutils tmux youtube-dl imagemagick gpg python3 postgres ffmpeg tree htop datamash; do
+for i in wget coreutils tmux youtube-dl imagemagick gpg python3 postgres ffmpeg tree htop datamash npm; do
 	echo "*** install "${i}" ***"; 
 	brew install ${i}; 
 done
@@ -15,12 +18,12 @@ echo "*** install pip ***"
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 
-echo "*** install virtualenv ***"
+echo "*** install python packages ***"
 sudo pip install virtualenv
-
-echo "*** install aws cli ***"
+pip install Pygments
 sudo pip3 install awscli --upgrade
-
-echo "*** install numpy, scipy ***"
 pip3 install numpy
 pip3 install scipy
+
+echo
+echo "Don't forget to download VLC, Office, Photoshop, Illustrator..."
